5cdeb953ae7ca3a480ccee1040a5e397daa1d6d5
inathanms <igornathandev@gmail.com>
2026-08-11 07:10:33 -0300

    feat: add card_tricks.go

---
+++ card_tricks/card_tricks.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	favoriteCards := []int{2, 6, 9}
+	return favoriteCards
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	if index >= len(slice) || index < 0 {
+		return -1
+	}
+	return slice[index]
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	lenSlice := len(slice)
+	if index == lenSlice || index == -1 {
+		slice = append(slice, value)
+		return slice
+	} else if index > len(slice) || index < -1 {
+		return slice
+	}
+
+	slice[index] = value
+	return slice
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	slice = append(values, slice...)
+	return slice
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index >= len(slice) || index < 0 {
+		return slice
+	}
+	slice = append(slice[0:index], slice[index+1:]...)
+	return slice
+}
+
+func main() {
+	fmt.Println(SetItem([]int{1, 2, 3}, -1, 4))
+}
